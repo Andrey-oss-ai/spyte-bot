317d5f78987c440f3f9e1c9a53cb18341541af47
andrey <66161872+Andrey-oss-ai@users.noreply.github.com>
2021-12-11 00:05:34 +0300

    Add parse steam news

---
+++ steam.py
@@ -0,0 +1,54 @@
+import requests
+import pickle, os
+from main import bot, dp, ROOT_DIR
+from bs4 import BeautifulSoup as BS
+
+URL = 'https://playisgame.com/halyava/steam'
+
+FILE = f'{ROOT_DIR}/steam_data.pickle'
+
+
+def parse_site():
+    page = requests.get(URL)
+    CODE: int = page.status_code
+    if CODE == 200:
+        html = BS(page.content, 'html.parser')
+        items = html.find_all(class_='pp-post-wrap pp-grid-item-wrap')
+        records = {}
+        for elem in items:
+            text = elem.find(class_="pp-post-title").find('a').get_text()
+            link = elem.find(class_="pp-post-title").find('a').get('href')
+            img_link = elem.find(class_='pp-post-thumbnail-wrap').find('img')['src']
+            records[text] = {'link': link, 'img': img_link}
+        return records
+
+
+def create_file():
+    with open(FILE, 'wb') as f:
+        pickle.dump(parse_site(), f)
+
+
+def read_file():
+    with open(FILE, 'rb') as f:
+        saved_records = pickle.load(f)
+        return saved_records
+
+
+async def send_steam_news():
+    all_records = parse_site()
+    try:
+        new_items = set(all_records) - set(read_file())
+    except:
+        create_file()
+        new_items = set(all_records) - set(read_file())
+    for item in new_items:
+        img_link = all_records[item]['img']
+        link = all_records[item]['link']
+        href = f'<a href="{link}">{item}</a>'
+
+        await bot.answer(
+            photo=img_link,
+            caption=href,
+            parse_mode='HTML'
+        )
+    create_file()
