9fcfa10af4ebc66a70510581fd20101fd83aafc9
antonov-andreu <andreu-antonov@yandex.ru>
2022-08-07 19:11:25 +0300

    add def send_of_startup

--- src/run_bot.py
+++ src/run_bot.py
@@ -5,4 +5,4 @@ import middlewares  # noqa
 from src.bot import dp
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=handlers.send_on_startup)
